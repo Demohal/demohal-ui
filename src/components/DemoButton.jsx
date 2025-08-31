ca71268de4d388d95f8dce67c3366206a7d02117
davidlbuch <dave@oppgen.net>
2025-08-31 16:26:01 -0400

    Update DemoButton.jsx

--- src/components/DemoButton.jsx
+++ src/components/DemoButton.jsx
@@ -1,17 +1,1 @@
 
-/* src/components/DemoButton.jsx */
-import React from "react";
-import styles from "./DemoButton.module.css";
-export default function DemoButton({ item, onClick, idx }) {
-  return (
-    <button
-      type="button"
-      onClick={onClick}
-      title={item.description || item.title}
-      className={styles.button}
-      data-idx={idx}
-    >
-      <div className={styles.label}>{item.title}</div>
-    </button>
-  );
-}
