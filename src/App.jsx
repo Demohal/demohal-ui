d4464ffcd1d35dee1d1875ce7fd51f0d1f36c226
davidlbuch <dave@oppgen.net>
2025-08-31 20:56:56 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import ShellEntry from "./components/ShellEntry";
-import RecoMock from "./features/reco/RecoMock";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ShellEntry />} />
-        <Route path="/reco-mock" element={<RecoMock />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
