0da62ef9fcf72c8d9a8848c09c5cc4127dcf77a9
davidlbuch <dave@oppgen.net>
2025-09-24 18:38:18 -0400

    Update ContentTemplate.jsx

--- src/components/views/ContentTemplate.jsx
+++ src/components/views/ContentTemplate.jsx
@@ -2,12 +2,11 @@
 // File: src/components/views/ContentTemplate.jsx
 import React from "react";
 
-
 export default function ContentTemplate({ title = "Template", children }) {
-return (
-<div className="text-[var(--message-fg,#111827)]">
-<div className="font-semibold mb-2">{title}</div>
-{children || <p>This is a content-only view. It does not know about the shell.</p>}
-</div>
-);
+  return (
+    <div className="text-[var(--message-fg,#111827)]">
+      <div className="font-semibold mb-2">{title}</div>
+      {children || <p>This is a content-only view. It does not know about the shell.</p>}
+    </div>
+  );
 }
