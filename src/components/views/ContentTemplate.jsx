da03e150c756efeb8488690d9a8307839a6de25a
davidlbuch <dave@oppgen.net>
2025-09-24 18:35:28 -0400

    Create ContentTemplate.jsx

---
+++ src/components/views/ContentTemplate.jsx
@@ -0,0 +1,13 @@
+// ---------------------------------------------
+// File: src/components/views/ContentTemplate.jsx
+import React from "react";
+
+
+export default function ContentTemplate({ title = "Template", children }) {
+return (
+<div className="text-[var(--message-fg,#111827)]">
+<div className="font-semibold mb-2">{title}</div>
+{children || <p>This is a content-only view. It does not know about the shell.</p>}
+</div>
+);
+}
