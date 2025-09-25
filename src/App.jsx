737e8e53f1d8794c6f66978fbe8af13a0d5d0528
davidlbuch <dave@oppgen.net>
2025-09-24 21:30:11 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,18 @@
-import ControlShell from "./components/ControlShell";
-export default function App(){ return <ControlShell />; }
+// src/App.jsx
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import AskAssistant from "./components/AskAssistant";
+import RecoMock from "./features/reco/RecoMock";  // ⬅️ new test page
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AskAssistant />} />
+        <Route path="/reco-mock" element={<RecoMock />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
