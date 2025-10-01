3456dda0f626ea9007efb428cbad4c33595a199b
davidlbuch <dave@oppgen.net>
2025-10-01 18:30:07 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// Vite config with React de-duplication so only one copy is bundled.
 export default defineConfig({
   plugins: [react()],
   resolve: {
