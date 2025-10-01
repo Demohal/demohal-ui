e598502a1380c8164d6cf8445779744148874480
davidlbuch <dave@oppgen.net>
2025-10-01 17:54:42 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Simple runtime check (harmless). You can remove later.
+if (typeof window !== 'undefined') {
+  try {
+    const hook = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+    const versions = hook
+      ? [...new Set(Object.values(hook.renderers || {}).map(r => r.version).filter(Boolean))]
+      : [];
+    console.log('[Runtime React versions]', versions);
+  } catch {
+    // ignore
+  }
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
