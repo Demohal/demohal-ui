fe27fc9c6ffa194f2e79a7cc582fc3dad79857c9
davidlbuch <dave@oppgen.net>
2025-10-04 16:34:03 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "./styles/tokens.css";
-import "./force-colors.css";   // <‑‑ ADD THIS LINE (must be last)
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
