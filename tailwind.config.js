ab65c53d0ca94a318eea2d625f8c2e89b757448f
davidlbuch <dave@oppgen.net>
2025-12-11 15:09:42 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     theme: {
         extend: {},
         fontSize: {
-            base: ['12pt', { lineHeight: '1.2' }],
+            base: ['14pt', { lineHeight: '1.4' }],
             sm: ['0.875rem', { lineHeight: '1.25rem' }],
             lg: ['1.125rem', { lineHeight: '1.75rem' }],
         },
