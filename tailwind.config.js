ac304bce2c3a1347fdb68b4a65d9f888df48ee71
davidlbuch <dave@oppgen.net>
2025-12-11 12:50:54 -0500

    Add custom font sizes to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {},
+        fontSize: {
+            base: ['12pt', { lineHeight: '1.2' }],
+            // Optional: keep other sizes if you use them
+            sm: ['0.875rem', { lineHeight: '1.25rem' }],
+            lg: ['1.125rem', { lineHeight: '1.75rem' }],
+            // ...
+        },
     },
     plugins: [],
 };
