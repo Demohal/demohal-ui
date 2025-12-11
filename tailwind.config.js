39cf9cdc9de204fc13c409a46b9cd0ccdb34bba3
davidlbuch <dave@oppgen.net>
2025-12-11 15:33:24 -0500

    Update base font line height in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     theme: {
         extend: {},
         fontSize: {
-            base: ['12pt', { lineHeight: '1.2' }],
+            base: ['12pt', { lineHeight: '1.4' }],
             sm: ['0.875rem', { lineHeight: '1.25rem' }],
             lg: ['1.125rem', { lineHeight: '1.75rem' }],
         },
@@ -13,7 +13,7 @@ module.exports = {
             addUtilities({
                 '.text-base-semibold': {
                     fontSize: '12pt',
-                    lineHeight: '1.2',
+                    lineHeight: '1.4',
                     fontWeight: '600', // semi-bold
                 }
             });
