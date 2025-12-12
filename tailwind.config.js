ed96096553e6df96a86634c9cd8b4ef9e45f52b2
davidlbuch <dave@oppgen.net>
2025-12-11 21:17:25 -0500

    Add custom font sizes and utility for semi-bold text

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {},
+        fontSize: {
+            base: ['12pt', { lineHeight: '1.4' }],
+            xs: ['0.750rem', { lineHeight: '1.00rem' }],
+            sm: ['0.875rem', { lineHeight: '1.25rem' }],
+            lg: ['1.125rem', { lineHeight: '1.75rem' }],
+            xl: ['1.250rem', { lineHeight: '2.00rem' }]
+        },
     },
-    plugins: [],
+    plugins: [
+        function ({ addUtilities }) {
+            addUtilities({
+                '.text-base-semibold': {
+                    fontSize: '12pt',
+                    lineHeight: '1.4',
+                    fontWeight: '600', // semi-bold
+                }
+            });
+        }
+    ],
 };
